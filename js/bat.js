0915f44c85371228256104c1fd80146a272af250
florinsamoila <33632959+florinsamoila@users.noreply.github.com>
2019-02-03 20:27:37 +0200

    Update bat.js

--- js/bat.js
+++ js/bat.js
@@ -1,6 +1,8 @@
 navigator.getBattery().then(function(battery) {
-
     var level = battery.level*100;
-
     document.querySelector('#level').textContent = level+"%";
+    battery.onchargingchange = chargingChange();
+   function chargingChange() {
+      document.querySelector('#chargingTime').textContent = battery.chargingTime;
+   }
 });
