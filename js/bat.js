9732b2e34c30ae0a86a01161cddb0071541a2ff0
florinsamoila <33632959+florinsamoila@users.noreply.github.com>
2019-02-03 20:56:35 +0200

    Update bat.js

--- js/bat.js
+++ js/bat.js
@@ -1,11 +1,19 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+
 navigator.getBattery().then(function(battery) {
     var level = battery.level*100;
     document.querySelector('#level').textContent = level+"%";
     var charging = battery.charging;
     if (charging==true){
-    document.querySelector('#charging').textContent = "Charging" ;
+        document.querySelector('#charging').textContent = "Charging" ;
     }
     if (charging==false){
-    document.querySelector('#charging').textContent = "Discharging" ;
+        document.querySelector('#charging').textContent = "Discharging" ;
+        ctx.beginPath();
+        ctx.lineWidth = "6";
+        ctx.strokeStyle = "red";
+        ctx.rect(5, 5, 290, 140); 
+        ctx.stroke();
     }
 });
